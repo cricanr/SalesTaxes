9f98a3fb7f79c76daabdab82818a3491b20d305e
Radu Crican <radu.crican@workday.com>
2018-06-13 15:37:40 +0200

    added tax calculator logic and tests - wip

--- src/main/scala/example/TaxCalculator.scala
+++ src/main/scala/example/TaxCalculator.scala
@@ -1,6 +1,21 @@
 package example
 
 class TaxCalculator {
+  def getTotalTaxes(inputLines: Seq[String]): Double = {
+    inputLines.map(inputLine => getTotalTax(inputLine)).sum
+  }
+
+  def getTotalTax(input: String): Double = {
+    Math.round(getImportTax(input).getOrElse(0.0D) + getSalesTax(input).getOrElse(0.0D))
+  }
+
+  def getImportTax(input: String): Option[Double] = {
+    if (input.toLowerCase.contains("imported")) {
+      val maybeProduct = Product(input)
+      maybeProduct.map(product => Math.round(product.price * 5 / 100 * 20.0).toDouble / 20.0)
+    } else None
+  }
+
   def getSalesTaxes(inputLines: Seq[String]): Double = {
     val taxCalculator = new TaxCalculator
     inputLines.map(inputLine => taxCalculator.getSalesTax(inputLine).getOrElse(0D)).sum
--- src/test/scala/example/TaxCalculatorTest.scala
+++ src/test/scala/example/TaxCalculatorTest.scala
@@ -34,4 +34,46 @@ class TaxCalculatorTest extends WordSpec with Matchers {
       }
     }
   }
+
+  "The Tax calculator getImportTax method" when {
+    "given the input '1 imported box of chocolates at 10.00'" should {
+      "return import tax 0.5" in {
+        val taxCalculator = new TaxCalculator()
+        taxCalculator.getImportTax("1 imported box of chocolates at 10.00") shouldEqual Some(0.5)
+      }
+    }
+  }
+
+  "The Tax calculator getTotalTax method" when {
+    "given the input '1 imported bottle of perfume at 27.99'" should {
+      "return total tax (sales and import) 4.0" in {
+        val taxCalculator = new TaxCalculator()
+        taxCalculator.getTotalTax("1 imported bottle of perfume at 27.99") shouldEqual 4.0
+      }
+    }
+  }
+
+  "The Tax calculator getTotalTaxes method" when {
+    "given multiple input lines" should {
+      "return total tax (sales and import)" in {
+        val taxCalculator = new TaxCalculator()
+        taxCalculator.getTotalTaxes(Seq("1 imported bottle of perfume at 27.99",
+          "1 bottle of perfume at 18.99",
+          "1 packet of headache pills at 9.75",
+          "1 box of imported chocolates at 11.25")) shouldEqual 7.0
+      }
+    }
+  }
+
+  "The Tax calculator getSalesTaxes method" when {
+    "given multiple input lines" should {
+      "return total sales taxes" in {
+        val taxCalculator = new TaxCalculator()
+        taxCalculator.getTotalTaxes(Seq("1 imported bottle of perfume at 27.99",
+          "1 bottle of perfume at 18.99",
+          "1 packet of headache pills at 9.75",
+          "1 box of imported chocolates at 11.25")) shouldEqual 6.70
+      }
+    }
+  }
 }
