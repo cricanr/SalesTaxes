b7d3676f9c05b9a18e4449175303c81094fe6dad
Radu Crican <radu.crican@workday.com>
2018-06-13 14:30:12 +0200

    added TaxCalculator logic and tests

--- src/main/scala/example/TaxCalculator.scala
+++ src/main/scala/example/TaxCalculator.scala
@@ -1,5 +1,9 @@
 package example
 
 class TaxCalculator {
+  def getSalesTax(input: String): Option[Double] = {
+    val maybeProduct = Product(input)
+    maybeProduct.collect { case product if product.hasSalesTax => Math.round(product.price * 10 / 100 * 20.0).toDouble / 20.0 }
+  }
 
 }
--- src/test/scala/example/TaxCalculatorTest.scala
+++ src/test/scala/example/TaxCalculatorTest.scala
@@ -1,7 +1,28 @@
 package example
 
-import org.scalatest.WordSpec
+import org.scalatest.{Matchers, WordSpec}
 
-class TaxCalculatorTest extends WordSpec {
+class TaxCalculatorTest extends WordSpec with Matchers {
+  "The Tax calculator getSalesTax method" when {
+    "given the input '1 book at 12.49'" should {
+      "return no sales tax" in {
+        val taxCalculator = new TaxCalculator()
+        taxCalculator.getSalesTax("1 book at 12.49") shouldEqual None
+      }
+    }
 
+    "given the input '1 music CD at 14.99'" should {
+      "return 1.50" in {
+        val taxCalculator = new TaxCalculator()
+        taxCalculator.getSalesTax("1 music CD at 14.99") shouldEqual Some(1.50)
+      }
+    }
+
+    "given the input '1 chocolate bar at 0.85'" should {
+      "return no sales tax" in {
+        val taxCalculator = new TaxCalculator()
+        taxCalculator.getSalesTax("1 chocolate bar at 0.85") shouldEqual None
+      }
+    }
+  }
 }
